0d537405c388022beb2c54e0e7e350fe1d711166
rdroid-27 <gupta27rachit@gmail.com>
2025-05-02 09:03:34 +0530

    Add proc file entries for user read calls.

--- Driver_101/ldd.c
+++ Driver_101/ldd.c
@@ -1,16 +1,46 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("rdroid-27 - Rachit");
 
-static int counter_module_init (void){
-    printk("Hello, World!\n");
+static struct proc_dir_entry *proc_entry;
+
+static ssize_t counter_read(struct file *file_pointer, char *user_space_buffer, size_t count, loff_t *offset)
+{
+    char mesg[] = "Ack!\n";
+    size_t len = strlen(mesg);
+    printk("Counter Read\n");
+    int result = copy_to_user(user_space_buffer, mesg, len);
+    if (*offset >= len)
+        return 0;
+    *offset += len;
+    return len;
+}
+
+static struct proc_ops driver_proc_ops{
+    .proc_read = counter_read};
+
+static int counter_module_init(void)
+{
+    printk("counter_module_init: Entry\n");
+    proc_entry = proc_create("counter_ldd", 0, NULL, &driver_proc_ops);
+    if (proc_entry == NULL)
+    {
+        printk("counter_module_init: Error\n");
+        return -1;
+    }
+    printk("counter_module_init: Exit\n");
+
     return 0;
 }
 
-static void counter_module_exit (void){
-    printk("Good Bye!\n");
+static void counter_module_exit(void)
+{
+    printk("counter_module_exit: Entry\n");
+    proc_remove(proc_entry);
+    printk("counter_module_exit: Exit\n");
 }
 
 module_init(counter_module_init);
